6c3d9c620cafc50de6ea7eff4a1a87c9f53579a2
Ben Kurtovic <ben.kurtovic@gmail.com>
2015-03-11 14:47:42 -0500

    Partial views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,17 @@
 module ApplicationHelper
 
   def get_title(title = '')
-    base = 'Calefaction'  # TODO: replace with settings name thing
+    base = '[Site Name]'
     title.empty? ? base : "#{title} : #{base}"
+  end
+
+  def get_copyright_year
+    start = 2015
+    year = Time.now.year
+    year > start ? "#{start}–#{year}" : start
+  end
+
+  def get_project_version
+    '0.1'  # todo
   end
 end
