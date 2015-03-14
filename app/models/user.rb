a87698d9b32769a0fee95338a92d6f1816ecaca1
Ben Kurtovic <ben.kurtovic@gmail.com>
2015-03-14 18:53:20 -0500

    Remove Encryptor; that was poorly thought-out...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,7 @@
 # require 'calefaction/api/eveonline'
 
 class User < ActiveRecord::Base
-  include Encryptor
   has_secure_password
-
-  def api_verify
-    self.class.decrypt(super())
-  end
-
-  def api_verify=(value)
-    super(self.class.encrypt(value))
-  end
 
   def member_of?(corp_id)
     false
